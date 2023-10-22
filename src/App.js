dcff06c988aeafe34c2df9607636e4ee99a3ae55
Varshita4466 <94522188+Varshita4466@users.noreply.github.com>
2023-10-22 17:15:35 +0530

    task6

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import About from './components/About';
+import Projects from './components/Project';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <About/>
+      <Projects/>
+
+
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Project.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <section id="projects">
+      <h2>Projects</h2>
+      To - Do List, One-page layout , Login authentication, Word Counter, Countdown timer
+      {/* Add your project components or details here */}
+    </section>
+  );
+}
+
+export default Projects;
